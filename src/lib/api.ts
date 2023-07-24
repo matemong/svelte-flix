2947c08a24a35443cf07a55eb58e6900c64a2c2f
Mate Mong <mongmate811@gmail.com>
2023-07-24 22:51:11 +0200

    hero component

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,5 @@
 export const base = 'https://api.movies.tastejs.com';
+export const media_base = 'https://image.tmdb.org/t/p';
 
 export async function get(
 	fetch: typeof globalThis.fetch,
@@ -9,4 +10,8 @@ export async function get(
 
 	const response = await fetch(`${base}/${endpoint}?${q}`);
 	return await response.json();
+}
+
+export function media(file_path: string, width: number) {
+	return `${media_base}/w${width}${file_path}`;
 }

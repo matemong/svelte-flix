f48bf45ba93167345aa48b9c6e55274e37e23de8
Mate Mong <mongmate811@gmail.com>
2023-07-24 22:31:21 +0200

    get featured movie

---
+++ src/routes/+page.ts
@@ -0,0 +1,14 @@
+import type { MovieDetails, MovieList } from '$lib/types.js';
+import * as api from '$lib/api';
+
+export async function load({ fetch }) {
+	const trending = (await api.get(fetch, 'trending/movie/day')) as MovieList;
+	const featured = (await api.get(fetch, `movie/${trending.results[0].id}`, {
+        append_to_response: 'images'
+    })) as MovieDetails;
+
+	return {
+		trending,
+		featured
+	};
+}

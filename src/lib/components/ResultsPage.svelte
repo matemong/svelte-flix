6f8b541fef62b0ccc8a08d007e93b1bc2278e6a4
Mate Mong <mongmate811@gmail.com>
2023-07-26 23:59:37 +0200

    scrolling

--- src/lib/components/ResultsPage.svelte
+++ src/lib/components/ResultsPage.svelte
@@ -1,31 +1,87 @@
 <script lang="ts">
 	import { media } from '$lib/api';
 	import type { MovieListResult } from '$lib/types';
+	import { createEventDispatcher, onMount } from 'svelte';
 
 	export let movies: MovieListResult[];
 	export let next: string | null;
+
+	const dispatch = createEventDispatcher();
+
+	let viewport: HTMLDivElement;
+	let results: HTMLDivElement;
+
+	let item_width: number;
+	let item_height: number;
+	let num_columns = 4;
+
+	let a: number = 0;
+	let b: number = movies.length;
+
+	let padding_top = 0;
+	let padding_bottom = 0;
+
+	function handle_resize() {
+		const first = results.firstChild as HTMLAnchorElement;
+
+		item_width = first.offsetWidth;
+		item_height = first.offsetHeight;
+
+		num_columns = 4;
+
+		handle_scroll();
+	}
+	function handle_scroll() {
+		const remaining = viewport.scrollHeight - (viewport.scrollTop + viewport.clientHeight);
+
+        if(remaining < 400){
+            dispatch('end');
+        }
+
+		a = Math.floor(viewport.scrollTop / item_height) * num_columns;
+		b = Math.ceil((viewport.scrollTop + viewport.clientHeight) / item_height) * num_columns;
+		padding_top = Math.floor(a / num_columns) * item_height;
+		padding_bottom = Math.floor((movies.length - b) / num_columns) * item_height;
+	}
+	onMount(handle_resize);
 </script>
 
-<div class="results">
-	{#each movies as movie}
-		<a href="/movies/{movie.id}">
-			<img src={media(movie.poster_path, 500)} alt={movie.title} />
-		</a>{/each}
+<svelte:window on:resize={handle_resize} />
+
+<p>showing items {a} - {b}</p>
+<div bind:this={viewport} class="viewport" on:scroll={handle_scroll}>
+	<div
+		bind:this={results}
+		class="results"
+		style:padding-top="{padding_top}px"
+		style:padding-bottom="{padding_bottom}px"
+	>
+		{#each movies.slice(a, b) as movie}
+			<a href="/movies/{movie.id}">
+				<img src={media(movie.poster_path, 500)} alt={movie.title} />
+			</a>{/each}
+	</div>
+	{#if next}
+		<a href={next}>next page</a>
+	{/if}
 </div>
 
-
 <style>
-    .results{
-        display: grid;
-        grid-template-columns: repeat(4, 1fr);
-        width: 100%;
-    }
-    a{
-        width: 100%;
-        aspect-ratio: 2/3;
-    }
+	.viewport {
+		height: 200px;
+		overflow-y: auto;
+	}
+	.results {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		width: 100%;
+	}
+	a {
+		width: 100%;
+		aspect-ratio: 2/3;
+	}
 
-    img{
-        width: 100%;
-    }
+	img {
+		width: 100%;
+	}
 </style>
--- src/routes/movies/[view=list]/+page.svelte
+++ src/routes/movies/[view=list]/+page.svelte
@@ -9,5 +9,8 @@
 	<ResultsPage
 		movies={data.movies}
 		next={data.next_page ? `/movies/${data.view}?page=${data.next_page}` : null}
-	/>
+        on:end={()=>{
+            console.log("todo");
+        }}
+        />
 </div>
